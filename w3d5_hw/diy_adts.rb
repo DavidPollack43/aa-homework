d59d651960ab666d55e64da719ebc284f09422c0
David <2018dpollack@gmail.com>
2023-08-03 23:41:34 -0700

    Added set and get methods

--- w3d5_hw/diy_adts.rb
+++ w3d5_hw/diy_adts.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Stack
     def initialize
         @stack = []
@@ -34,4 +35,43 @@ class Queue
         @queue[-1]
     end
 end
+
+class Map
+    def initialize
+        @map = []
+    end
+
+    def set(key,value)
+        key_include = false
+        pair_index = nil
+        # debugger
+        @map.each_with_index do |pair, inx| 
+            key_include = true if pair[0] == key
+            pair_index = inx if key_include && pair_index == nil
+        end
+        if !key_include
+            @map << [key, value]
+        else
+            @map.delete_at(pair_index)
+            @map << [key,value]
+        end
+        return @map
+    end
+
+    def get(key)
+        @map.each_with_index do |pair, i|
+            return pair if pair[0] == key
+        end
+        return nil
+    end
+end
+
+m = Map.new
+m.set(1,1)
+m.set(2,2)
+m.set(1,4)
+p m
+p m.get(2)
+p m.get(1)
+p m.get(0)
 

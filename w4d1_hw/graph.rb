ed717a0a1be1b871ea6ead8674d78f7c20627a57
David <2018dpollack@gmail.com>
2023-08-04 21:39:27 -0700

    finished graphNode class and put in bfs

--- w4d1_hw/graph.rb
+++ w4d1_hw/graph.rb
@@ -1,3 +1,4 @@
+require 'set'
 class GraphNode
     attr_reader :value, :neighbors
     def initialize(value, neighbors = [])
@@ -11,5 +12,19 @@ class GraphNode
 
     def inspect
         return "ID: #{self.object_id} Value: #{self.value}"
+    end
+
+    def bfs(starting_node, target_value)
+        queue = [starting_node]
+        set = Set.new()
+        while !queue.empty?
+            deq_ele = queue.shift 
+            return deq_ele if deq_ele.value == target_value
+            set.add(deq_ele)
+            deq_ele.neighbors.each do |n_node|
+                queue << n_node if !set.include?(n_node)
+            end
+        end
+        nil
     end
 end

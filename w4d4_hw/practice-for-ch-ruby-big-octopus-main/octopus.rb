df69aaed3a435160b66b99c81286504082719854
David <2018dpollack@gmail.com>
2023-08-09 22:42:01 -0700

    finished hw

--- w4d4_hw/practice-for-ch-ruby-big-octopus-main/octopus.rb
+++ w4d4_hw/practice-for-ch-ruby-big-octopus-main/octopus.rb
@@ -43,3 +43,23 @@ def clever_octo(array)
     max_word
 end     
 
+def slow_dance(move, tiles_array)
+    indx = tiles_array.index(move)
+    tiles_array.each_with_index do |ele, i|
+        return i if i == indx
+    end 
+    nil   
+end    
+
+def fast_dance(move, tiles)
+    return tiles[move]
+end    
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", 
+    "left",  "left-up"]
+
+tiles = {"up" => 0, "right-up" => 1, "right" => 2, "right-down" => 3, "down" => 4, "left-down" => 5, 
+        "left" => 6, "left-up" => 7}
+p fast_dance("up", tiles)
+
+p fast_dance("right-down", tiles)
